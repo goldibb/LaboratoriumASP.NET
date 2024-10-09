c14d58ed5fde07143d9775938458cd647e8cbf6d
goldibb <igornejman13@gmail.com>
2024-10-09 12:10:57 +0200

    Added Calculator

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -22,14 +22,57 @@ public class HomeController : Controller
         return View();
     }
     
-    public IActionResult Calculator()
+    public IActionResult Calculator(Operator? op, double? x, double? y)
     {
+        //var op = Request.Query["op"];
+        //var x = double.Parse(Request.Query["x"]);
+        //var y = double.Parse(Request.Query["y"]);
+        if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby x lub y lub ich brak!";
+            return View("CalculatorError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator";
+            return View("CalculatorError");
+        }
+        double? result = 0.0d;
+        switch (op)
+        {
+            case Operator.Add:
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case Operator.Sub:
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case Operator.Mul:
+                ViewBag.Operator = "*";
+                result = x * y;
+                break;
+            case Operator.Div:
+                ViewBag.Operator = "/";
+                result = x / y;
+                break;
+        }
+
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
         return View();
     }
 
     public IActionResult Privacy()
     {
         return View();
+    }
+    
+    public enum Operator
+    {
+        Add,Sub,Mul,Div
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
